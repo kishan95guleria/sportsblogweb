6fae27e8f7be4bc6c54709772b20f0694e2371c4
kishan95guleria <>
2021-10-06 10:23:53 +0530

    second commit

--- sportsblog/settings.py
+++ sportsblog/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k#2jcv)efqp*+$zwfc9^fj@jr4j1o&ws-33dcmj=!jkm!=y*rf'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sportsbreed.herokuapp.com", "localhost"]
 
 
 # Application definition
